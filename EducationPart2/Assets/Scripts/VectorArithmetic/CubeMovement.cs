6b2b5b84849b57ed60a37b45eb2c5c8858e1b044
Evg Pn <ye.ponomarenko@gmail.com>
2020-03-23 12:24:33 +0200

    feat(project) create VectorArithmetic scene
    VectorArithmetic scene has Cube GameObjet that can move in selected direction represented as a Vector

---
+++ EducationPart2/Assets/Scripts/VectorArithmetic/CubeMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _moveDirection = new Vector3(0, 0, 0); //e.g. (1,0,0)
+    private float _moveSpeed = 2f;
+
+    private void FixedUpdate()
+    {
+        transform.position += _moveDirection * _moveSpeed * Time.deltaTime;
+    }
+}
