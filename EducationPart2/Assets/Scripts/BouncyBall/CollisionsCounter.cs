22524126e1180fb9f3503cd5e85a98abc45b9fd2
Evg Pn <ye.ponomarenko@gmail.com>
2020-03-23 17:30:59 +0200

    feat(project) create BouncyBall scene
    Scene has bouncing ball and three counters(distance between ground and ball, amount of bounces, amount of triggering of platform under ball)

---
+++ EducationPart2/Assets/Scripts/BouncyBall/CollisionsCounter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionsCounter : MonoBehaviour
+{
+    [SerializeField]
+    private Text _bounceCounterUI;
+
+    [SerializeField]
+    private Text _passCounterUI;
+
+    private float _bounceCounter = 0f;
+    private float _passCounter = 0f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            if(_bounceCounterUI != null)
+            {
+                _bounceCounter++;
+                _bounceCounterUI.text = _bounceCounter.ToString();
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            if (_passCounterUI != null)
+            {
+                _passCounter++;
+                _passCounterUI.text = _passCounter.ToString();
+            }
+        }
+    }
+}
---
+++ EducationPart2/Assets/Scripts/BouncyBall/DistanceCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceCounter : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _platform;
+    [SerializeField]
+    private GameObject _ball;
+    [SerializeField]
+    private float _distance = 0f;
+    [SerializeField]
+    private Text _distanceUI;
+
+    private void FixedUpdate()
+    {
+        _distance = _ball.transform.position.y - _platform.transform.position.y;
+        _distanceUI.text = _distance.ToString();
+    }
+}
