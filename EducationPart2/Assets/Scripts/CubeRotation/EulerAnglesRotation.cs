23711300af39443cde8cb9b5c4c496ed87cb6d99
Evg Pn <ye.ponomarenko@gmail.com>
2020-03-23 11:41:07 +0200

    feat(project) create CubeRotation scene
    CubeRotation scene have Cube GameObject that rotates with selected RPM

---
+++ EducationPart2/Assets/Scripts/CubeRotation/EulerAnglesRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EulerAnglesRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float _rpm = 60f;
+
+    private void FixedUpdate()
+    {
+        transform.eulerAngles += Vector3.up * Time.deltaTime * (_rpm * 6f);
+    }
+}
