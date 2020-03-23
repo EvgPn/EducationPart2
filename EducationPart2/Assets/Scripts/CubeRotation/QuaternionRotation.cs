629a2a0d8ac3d1e2f09c6186000985a5a47f7500
Evg Pn <ye.ponomarenko@gmail.com>
2020-03-23 17:59:42 +0200

    feat(CubeRotation Scene) add cube with quaternion rotation
    added cube rotates around first cube with selected RPM

---
+++ EducationPart2/Assets/Scripts/CubeRotation/QuaternionRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class QuaternionRotation : MonoBehaviour
+{
+    [SerializeField]
+    private float _rpm = 60f;
+    [SerializeField]
+    private Transform _centerOfRotation;
+    private Vector3 _v;
+    private void Start()
+    {
+        _v = transform.position - _centerOfRotation.position;
+    }
+    private void FixedUpdate()
+    {
+        _v = Quaternion.AngleAxis(Time.deltaTime * _rpm * 6f, Vector3.up) * _v;
+        transform.position = _centerOfRotation.position + _v; 
+    }
+}
